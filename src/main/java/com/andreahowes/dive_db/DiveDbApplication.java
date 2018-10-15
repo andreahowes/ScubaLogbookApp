3ae84ebfa3a51127e52c4be12029d753f6f30ee1
andreahowes <ashcreek.howes@gmailcom>
2018-10-15 12:28:15 -0500

    first commit

---
+++ src/main/java/com/andreahowes/dive_db/DiveDbApplication.java
@@ -0,0 +1,12 @@
+package com.andreahowes.dive_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiveDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiveDbApplication.class, args);
+    }
+}
